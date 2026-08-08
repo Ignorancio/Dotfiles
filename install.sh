c387c959a905a90f58fa97f1c88502c7a6e75679
Ignorancio <cruzsuarezbrandon@gmail.com>
2026-08-08 16:07:07 -0500

    add pacman and aur package lists for installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [ ! -f "terminal" ]; then
+if [ ! -f "pacman.txt" ]; then
   echo "Packages file not found!"
+  exit
+fi
+
+if [ ! -f "aur.txt" ]; then
+  echo "AUR packages file not found!"
   exit
 fi
 
@@ -9,7 +14,7 @@ echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
 echo "Installing packages..."
-sudo pacman -S --needed --noconfirm - < terminal
+sudo pacman -S --needed --noconfirm - < pacman.txt
 
 echo "Copy configuration files..."
 cp -rf .config/ ~/
@@ -18,8 +23,19 @@ cp .config/.zshrc ~/
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+cd ..
+yay -S < aur.txt
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
+
+sudo systemctl enable ly@tty2
 
 echo "Installation complete!"
